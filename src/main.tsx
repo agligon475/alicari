335954e86aa65ac05ccb38e7fa1320a729a8eb9b
agligon475 <panlactal@gmail.com>
2026-06-29 22:34:11 -0300

    fix: evitar error de render de React al hacer los mount points condicionales en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,20 @@ import DemoOne from './components/demo'
 import { BackgroundGradientAnimationDemo } from './components/cta-demo'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('hero-root') as HTMLElement).render(
-  <React.StrictMode>
-    <DemoOne />
-  </React.StrictMode>,
-)
+const heroRoot = document.getElementById('hero-root');
+if (heroRoot) {
+  ReactDOM.createRoot(heroRoot).render(
+    <React.StrictMode>
+      <DemoOne />
+    </React.StrictMode>,
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('cta-root') as HTMLElement).render(
-  <React.StrictMode>
-    <BackgroundGradientAnimationDemo />
-  </React.StrictMode>,
-)
+const ctaRoot = document.getElementById('cta-root');
+if (ctaRoot) {
+  ReactDOM.createRoot(ctaRoot).render(
+    <React.StrictMode>
+      <BackgroundGradientAnimationDemo />
+    </React.StrictMode>,
+  );
+}
